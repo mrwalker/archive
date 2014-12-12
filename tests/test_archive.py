10431aa76e666b423e655e3839b0beb62b5c42d0
Matt Walker <matt.r.walker@gmail.com>
2014-12-12 15:38:29 -0600

    PEP 8 whitespace in tests

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -7,58 +7,58 @@ from archive.archive import Archive
 from archive.relation import ExternalTable, ViewUntilTable
 
 class TestArchive:
-  def setup(self):
-    self.archive = Archive('tests', Hive())
-    self.events = ExternalTable(
-      'atomic',
-      'events',
-      partitioned = True
-    )
+    def setup(self):
+        self.archive = Archive('tests', Hive())
+        self.events = ExternalTable(
+            'atomic',
+            'events',
+            partitioned=True
+        )
 
-  def test_lookup(self):
-    self.archive.add(self.events)
-    events = self.archive.lookup('events')
+    def test_lookup(self):
+        self.archive.add(self.events)
+        events = self.archive.lookup('events')
 
-    assert_is_not_none(events)
-    assert_equal('events', events.name)
-    assert_equal(self.events, events)
+        assert_is_not_none(events)
+        assert_equal('events', events.name)
+        assert_equal(self.events, events)
 
-  @raises(KeyError)
-  def test_missing_lookup(self):
-    self.archive.lookup('doesnotexist')
+    @raises(KeyError)
+    def test_missing_lookup(self):
+        self.archive.lookup('doesnotexist')
 
-  @raises(RuntimeError)
-  def test_unique_names(self):
-    self.archive.add(self.events)
-    self.archive.add(self.events)
+    @raises(RuntimeError)
+    def test_unique_names(self):
+        self.archive.add(self.events)
+        self.archive.add(self.events)
 
-  @raises(RuntimeError)
-  def test_missing_input(self):
-    self.archive.add(self.events)
-    doesnotexist = ViewUntilTable(
-      'atomic',
-      'doesnotexist',
-      self.events
-    )
-    self.archive.add(ViewUntilTable(
-      'atomic',
-      'missing_input',
-      doesnotexist
-    ))
+    @raises(RuntimeError)
+    def test_missing_input(self):
+        self.archive.add(self.events)
+        doesnotexist = ViewUntilTable(
+            'atomic',
+            'doesnotexist',
+            self.events
+        )
+        self.archive.add(ViewUntilTable(
+            'atomic',
+            'missing_input',
+            doesnotexist
+        ))
 
-  def test_graph(self):
-    self.archive.add(self.events)
-    assert_equal('Archive: tests\nExternalTable(atomic.events)', self.archive.graph())
+    def test_graph(self):
+        self.archive.add(self.events)
+        assert_equal('Archive: tests\nExternalTable(atomic.events)', self.archive.graph())
 
-  def test_stats(self):
-    self.archive.add(self.events)
-    stats = self.archive.optimize()
-    assert_equal(1, stats['archive']['databases'])
-    assert_equal(1, stats['archive']['depth'])
-    assert_equal(1, stats['archive']['queries'])
+    def test_stats(self):
+        self.archive.add(self.events)
+        stats = self.archive.optimize()
+        assert_equal(1, stats['archive']['databases'])
+        assert_equal(1, stats['archive']['depth'])
+        assert_equal(1, stats['archive']['queries'])
 
-    assert_equal(1, stats['databases']['references']['atomic'])
-    assert_equal(set(['atomic']), stats['databases']['unique_databases'])
+        assert_equal(1, stats['databases']['references']['atomic'])
+        assert_equal(set(['atomic']), stats['databases']['unique_databases'])
 
-    assert_equal(1, stats['queries']['references']['events'])
-    assert_equal(set(['events']), stats['queries']['unique_queries'])
+        assert_equal(1, stats['queries']['references']['events'])
+        assert_equal(set(['events']), stats['queries']['unique_queries'])
--- tests/test_external_table.py
+++ tests/test_external_table.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 import re
 
 from nose.tools import *
-
 from jinja2 import TemplateNotFound
 
 from archive.hive import Hive
@@ -11,56 +10,56 @@ from archive.archive import Archive
 from archive.relation import ExternalTable
 
 class TestExternalTable:
-  def setup(self):
-    self.archive = Archive('tests', Hive())
+    def setup(self):
+        self.archive = Archive('tests', Hive())
 
-  @raises(TemplateNotFound)
-  def test_missing_hql(self):
-    misnamed = self.archive.add(ExternalTable(
-      'atomic',
-      'misnamed'
-    ))
-    misnamed.hql()
+    @raises(TemplateNotFound)
+    def test_missing_hql(self):
+        misnamed = self.archive.add(ExternalTable(
+            'atomic',
+            'misnamed'
+        ))
+        misnamed.hql()
 
-  def test_graph(self):
-    events = self.archive.add(ExternalTable(
-      'atomic',
-      'events',
-      partitioned = True
-    ))
-    assert_equal('ExternalTable(atomic.events)', events.graph())
+    def test_graph(self):
+        events = self.archive.add(ExternalTable(
+            'atomic',
+            'events',
+            partitioned=True
+        ))
+        assert_equal('ExternalTable(atomic.events)', events.graph())
 
-  def test_stats(self):
-    events = self.archive.add(ExternalTable(
-      'atomic',
-      'events',
-      partitioned = True
-    ))
-    stats = self.archive.optimize()
-    assert_equal(1, stats['archive']['databases'])
-    assert_equal(1, stats['archive']['depth'])
-    assert_equal(1, stats['archive']['queries'])
+    def test_stats(self):
+        events = self.archive.add(ExternalTable(
+            'atomic',
+            'events',
+            partitioned=True
+        ))
+        stats = self.archive.optimize()
+        assert_equal(1, stats['archive']['databases'])
+        assert_equal(1, stats['archive']['depth'])
+        assert_equal(1, stats['archive']['queries'])
 
-    assert_equal(1, stats['databases']['references']['atomic'])
-    assert_equal(set(['atomic']), stats['databases']['unique_databases'])
+        assert_equal(1, stats['databases']['references']['atomic'])
+        assert_equal(set(['atomic']), stats['databases']['unique_databases'])
 
-    assert_equal(1, stats['queries']['references']['events'])
-    assert_equal(set(['events']), stats['queries']['unique_queries'])
+        assert_equal(1, stats['queries']['references']['events'])
+        assert_equal(set(['events']), stats['queries']['unique_queries'])
 
-  def test_partitioning(self):
-    pattern = re.compile('.*RECOVER PARTITIONS', re.DOTALL)
+    def test_partitioning(self):
+        pattern = re.compile('.*RECOVER PARTITIONS', re.DOTALL)
 
-    events = self.archive.add(ExternalTable(
-      'atomic',
-      'events',
-      partitioned = True
-    ))
-    assert_true(pattern.match(events.create_hql()))
+        events = self.archive.add(ExternalTable(
+            'atomic',
+            'events',
+            partitioned=True
+        ))
+        assert_true(pattern.match(events.create_hql()))
 
-    # Reset Archive
-    self.setup()
-    events = self.archive.add(ExternalTable(
-      'atomic',
-      'events'
-    ))
-    assert_is_none(pattern.match(events.create_hql()))
+        # Reset Archive
+        self.setup()
+        events = self.archive.add(ExternalTable(
+            'atomic',
+            'events'
+        ))
+        assert_is_none(pattern.match(events.create_hql()))
